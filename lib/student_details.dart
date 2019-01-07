b5fa7580a69cac4f9e1e87e76942abad40a51a61
jorgechavezrnd <jorgechavezrnd@gmail.com>
2019-01-07 14:15:26 -0400

    Aplicacion funcionando en ios

---
+++ lib/student_details.dart
@@ -0,0 +1,125 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:aplicacion_seminario/models/student_model.dart';
+
+//const apiUrl = 'http://192.168.133.129:8080/compare';
+const apiUrl = 'http://192.168.133.129:3000/api/upload';
+//const pictureUrl = 'assets/img/nobody.jpg';
+const pictureUrl = 'http://192.168.133.129:3000/image1.png';
+
+class StudentDetails extends StatefulWidget {
+
+  File image;
+
+  StudentDetails({ this.image });
+
+  @override
+  _StudentDetailsState createState() {
+    return _StudentDetailsState();
+  }
+
+}
+
+class _StudentDetailsState extends State<StudentDetails> {
+  dynamic response;
+  List<StudentModel> students;
+
+  @override
+  void initState() {
+    super.initState();
+    this.sendImage();
+  }
+
+  /*void sendImage() {
+    print('Send Image');
+
+    Dio dio = new Dio();
+    FormData formData = new FormData();
+    formData.add('file', new UploadFileInfo(widget.image, basename(widget.image.path)));
+
+    dio.post(apiUrl, data: formData, options: Options(
+      method: 'POST',
+      responseType: ResponseType.JSON
+    )).then((response) => setState(() {
+      this.response = response;
+      this.students = new List();
+
+      int tam = this.response.data['students'].length;
+
+      for (int i = 0; i < tam; ++i) {
+        String name = this.response.data['students'][i];
+        print('Student $i : $name');
+        this.students.add(StudentModel(name: name, pictureUrl: pictureUrl));
+      }
+
+    })).catchError((error) => print(error));
+  }*/
+
+  void sendImage() async {
+    print('Send Image');
+
+    List<int> imageBytes = widget.image.readAsBytesSync();
+    String base64Image = base64Encode(imageBytes);
+
+    Map map = {
+      'image': base64Image
+    };
+
+    HttpClient httpClient = new HttpClient();
+    HttpClientRequest request = await httpClient.postUrl(Uri.parse(apiUrl));
+    request.headers.set('content-type', 'application/json');
+    request.add(utf8.encode(json.encode(map)));
+    HttpClientResponse response = await request.close();
+    String reply = await response.transform(utf8.decoder).join();
+    httpClient.close();
+    var replyJson = jsonDecode(reply);
+
+    setState(() {
+      this.students = new List();
+
+      int tam = replyJson['students'].length;
+
+      for (int i = 0; i < tam; ++i) {
+        String name = replyJson['students'][i];
+        this.students.add(StudentModel(name: name, pictureUrl: pictureUrl));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Estudiantes presentes'),
+        ),
+      ),
+      //body: response == null ? Center(child: CircularProgressIndicator()) : Text(this.response.data['students'].toString())
+      body: this.students == null ? Center(child: CircularProgressIndicator(),) : ListView.builder(
+        itemCount: this.students.length,
+        itemBuilder: (context, i) => Column(
+          children: <Widget>[
+            Divider(
+              height: 10.0,
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                foregroundColor: Theme.of(context).primaryColor,
+                backgroundColor: Colors.grey,
+                // backgroundImage: AssetImage(this.students[i].pictureUrl),
+                backgroundImage: NetworkImage(pictureUrl),
+              ),
+              title: Text(
+                this.students[i].name,
+                style: TextStyle(fontWeight: FontWeight.bold)
+              )
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+}
---
+++ lib/take_picture.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
+import 'package:aplicacion_seminario/student_details.dart';
+import 'dart:io';
+import 'dart:convert';
+
+class TakePicture extends StatefulWidget {
+  @override
+  _TakePictureState createState() {
+    return _TakePictureState();
+  }
+}
+
+class _TakePictureState extends State<TakePicture> {
+  File image;
+  SocketIO socketIO;
+
+  void takePicture(String source) async {
+    ImageSource imageSource;
+
+    if (source == 'camera') {
+      print('Camera');
+      imageSource = ImageSource.camera;
+    } else {
+      print('Gallery');
+      imageSource = ImageSource.gallery;
+    }
+
+    File img = await ImagePicker.pickImage(source: imageSource);
+
+    if (img != null) {
+      setState(() {
+        this.image = img;
+        this.image.length().then((len) {
+          print('Tamanio: $len');
+        });
+      });
+    }
+
+    socketIO = SocketIOManager().createSocketIO("http://10.0.0.17:3000", "/", query: '');
+
+    socketIO.init();
+
+    socketIO.subscribe("message", (data){ print('FUNCIONA!!!!!!!'); print(data); });
+
+    socketIO.connect();
+  }
+
+  void pushStudentsDetails(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => StudentDetails(image: this.image)
+      )
+    );
+  }
+
+  void sendImage() {
+    List<int> imageBytes = this.image.readAsBytesSync();
+    String base64Image = base64Encode(imageBytes);
+
+    socketIO.sendMessage('upload', '{"image": "$base64Image"}', (){});
+
+    socketIO.disconnect();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Escoger Imagen'),
+        ),
+      ),
+      body: Container(
+        child: Center(
+            child: this.image == null
+                ? Text('Imagen no seleccionada')
+                : Image.file(this.image)),
+      ),
+      bottomNavigationBar: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            FloatingActionButton(
+                heroTag: 1,
+                // onPressed: () => takePicture('camera'),
+                onPressed: () => this.sendImage(),
+                child: Icon(Icons.camera_alt)),
+            FloatingActionButton(
+                heroTag: 2,
+                onPressed: () =>
+                    image == null ? null : pushStudentsDetails(context),
+                child: Icon(Icons.send),
+                backgroundColor: this.image == null ? Colors.grey : null),
+            FloatingActionButton(
+                heroTag: 3,
+                onPressed: () => takePicture('gallery'),
+                child: Icon(Icons.image))
+          ],
+        ),
+      ),
+    );
+  }
+}
